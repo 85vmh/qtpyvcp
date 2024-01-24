db73e3077e3d81496f096a0f11f4751979be453d
Vasi Mihalca <vasi.mihalca@ww.com>
2024-01-24 14:14:56 +0200

    Changed signal name for TeachInButton

--- src/qtpyvcp/widgets/input_widgets/teachin_button.py
+++ src/qtpyvcp/widgets/input_widgets/teachin_button.py
@@ -5,7 +5,7 @@ TeachInButton
 """
 
 from qtpy.QtWidgets import QPushButton
-from qtpy.QtCore import Slot, Property, Signal
+from qtpy.QtCore import Signal
 
 from qtpyvcp.widgets.base_widgets.dro_base_widget import DROBaseWidget
 
@@ -13,12 +13,12 @@ from qtpyvcp.widgets.base_widgets.dro_base_widget import DROBaseWidget
 class TeachInButton(QPushButton, DROBaseWidget):
     """TeachInButton
     """
-    currentValue = Signal(float)
+    onValueCaptured = Signal(float)
 
     def __init__(self, parent=None):
         super(TeachInButton, self).__init__(parent)
         self.current_value = 0
-        self.clicked.connect(lambda: self.currentValue.emit(self.current_value))
+        self.clicked.connect(lambda: self.onValueCaptured.emit(self.current_value))
 
     def setValue(self, value):
         self.current_value = float(value)
